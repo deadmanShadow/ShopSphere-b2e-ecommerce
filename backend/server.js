529cecc65eacc33706dd54c348a57aab9b4430ea
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-18 00:19:48 +0600

    add

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
-
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,4 +13,8 @@ app.use("/api/auth", authRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on http://localhost:" + PORT);
+    connectDB();
 })
+
+
+// Wb8LD16Ly1rdrbBy
