7afc7ac3265a6246305fdd82d8d257ee19a6cc8f
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-17 21:02:15 +0600

    project setup

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+router.get("/signup",(req,res)=>{
+    res.send("Sign up route called");
+})
+router.get("/login",(req,res)=>{
+    res.send("Login route called");
+})
+router.get("/logout",(req,res)=>{
+    res.send("Logout route called");
+})
+export default router;
