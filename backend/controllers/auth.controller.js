5272ef6cdcca3686672097339700e2d75ec3ce87
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-18 22:04:52 +0600

    add

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,32 @@
-export const signup = async (req,res)=>{
-    res.send("Sign up route called");
-}
-export const login = async (req,res)=>{
-    res.send("login route called");
-}
-export const logout = async (req,res)=>{
-    res.send("logout route called");
-}
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    const user = await User.create({ name, email, password });
+
+    //auth user
+    
+
+    res.status(201).json({ user, message: "User created" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const login = async (req, res) => {
+  res.send("login route called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route called");
+};
