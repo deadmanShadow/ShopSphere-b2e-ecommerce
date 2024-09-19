a686f2bac90dde16b3f387e05af97f702cfecda4
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-19 20:30:32 +0600

    coupon add

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on http://localhost:" + PORT);
