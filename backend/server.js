c51d96ae7834da241a885bf3f63c6e0600ba7b71
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-19 19:46:03 +0600

    cart access

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import productRoutes from "./routes/product.route.js";
 dotenv.config();
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on http://localhost:" + PORT);
