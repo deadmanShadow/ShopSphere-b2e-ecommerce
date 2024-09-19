42381c15e1b2bdc505e2d663dabf571e318bf75c
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-19 14:09:55 +0600

    auth middle

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
-
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on http://localhost:" + PORT);
